3a6ca977c3e72f4323f6cd02d7b3cc94e420906b
ioannis <dev@iamioannis.com>
2014-04-11 17:09:34 +0100

    Original commit with the project

---
+++ src/jmetrics/codeCounter.java
@@ -0,0 +1,46 @@
+/**
+ * Created by ioannis on 11/04/2014.
+ */
+package jmetrics;
+
+/**
+ *
+ * @author ioannis
+ */
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public final class codeCounter {
+
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+        }
+        else {
+            codeCounter loc = new codeCounter();
+            File root = new File(args[0]);
+            loc.countLinesOfCode(root);
+            System.out.println("Found " + loc.lines + " lines of code in "+ root.getName() + " in " + root.getCanonicalPath());
+        }
+    }
+
+    private long lines = 0;
+
+    private void countLinesOfCode(File f) throws IOException {
+
+        if (f.getName().endsWith(".java")) {
+            try (BufferedReader br = new BufferedReader(new FileReader(f))) {
+                String line = br.readLine();
+                while (line != null) {
+                    if (!line.trim().equals("")) {
+                        this.lines++;
+                    }
+                    line = br.readLine();
+                }
+            }
+        }
+    }
+}
+
