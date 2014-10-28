c0faa5e65be8f093abed3288f2fd2c4e3ade142b
ioannis <dev@iamioannis.com>
2014-10-28 12:55:23 +0000

    Licence and fixes
    Added: GNU General Public Licence

--- src/ASTOperations/ASTImpl.java
+++ src/ASTOperations/ASTImpl.java
@@ -1,7 +1,19 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2014 Ioannis Mastigopoulos
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
 package ASTOperations;
@@ -16,7 +28,7 @@ import javax.tools.StandardJavaFileManager;
 import javax.tools.ToolProvider;
 /**
  *
- * @author ioannis
+ * @author Ioannis Mastigopoulos
  */
 public class ASTImpl {
     /** 
@@ -29,7 +41,7 @@ public class ASTImpl {
         
         //Initation of the compilation process of the file
         JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-        DiagnosticCollector<JavaFileObject> diagnosticsCollector = new DiagnosticCollector<JavaFileObject>();
+        DiagnosticCollector<JavaFileObject> diagnosticsCollector = new DiagnosticCollector<>();
         //Standar file manager generation from the compiler to handle file
         StandardJavaFileManager fileManager = compiler.getStandardFileManager(diagnosticsCollector, null, null);
         //Collection of the files objects
